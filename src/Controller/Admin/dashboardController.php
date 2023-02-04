8e07dfbb34888df36f5014a7c507c937b2af173c
Elisee-des <esabidani@gmail.com>
2023-02-04 10:17:54 -0800

    Installation du template pour l'administrateur et l'utilisateur

--- src/Controller/Admin/dashboardController.php
+++ src/Controller/Admin/dashboardController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'admin_')]
 class dashboardController extends AbstractController
 {
-    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
         return $this->render('admin/dashboard/index.html.twig', [
